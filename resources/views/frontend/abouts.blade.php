e61c77fc9371dd58a8d3347262ef8ca5331122b0
Fatoumata Traore <fatoumata065@gmail.com>
2023-12-27 16:29:41 +0000

    video

--- resources/views/frontend/abouts.blade.php
+++ resources/views/frontend/abouts.blade.php
@@ -91,12 +91,12 @@
       </div> --}}
 
     <div class="row">
-      <div class="item item--1 col-md-6">
-        <video class="vid" controls src="video/vfr.mp4" width="100%" height="360" ></video>
+      <div class="item item--1 col-md-6" >
+        <video  class="vid" controls src="video/vfr.mp4" width="100%" height="100%" style="border:0.2rem thick triple #253e77; border-radius:10px;" ></video>
       </div>
 
       <div class="item item--2 col-md-6">
-        <video  class="vid" controls src="video/pays.mp4" width="100%" height="360" webkitallowfullscreen mozallowfullscreen allowfullscreen></video>
+        <video  class="vid" controls src="video/pays.mp4" width="100%" height="100%" style="border:2rem thick triple #253e77; border-radius:10px;" ></video>
       </div>
     </div>
 
