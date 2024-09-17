c23b952313b3b994d6c9b76249708d750a5fc7ee
Fatoumata Traore <fatoumata065@gmail.com>
2024-09-17 20:42:45 +0000

    changements dans contact

--- resources/views/frontend/abouts.blade.php
+++ resources/views/frontend/abouts.blade.php
@@ -120,7 +120,7 @@
             <div class="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.1s">
                 <i class="fa fa-certificate fa-3x text-secondary mb-3"></i>
                 <h1 class="text-white mb-2" data-toggle="counter-up">2 </h1>
-                <p class="text-white mb-0">Annéés d'expériences</p>
+                <p class="text-white mb-0">Années d'expériences</p>
             </div>
             <div class="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.3s">
                 <i class="fa fa-users-cog fa-3x text-secondary mb-3"></i>
